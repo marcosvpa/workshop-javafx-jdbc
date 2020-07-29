28703cf0af19fa9c66852bdaf1d81fd19695ddd1
Marcos Vinicius <marcos_andrade9@hotmail.com>
2020-07-28 21:42:20 -0300

    Adding database access

---
+++ src/db/DbExeception.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DbExeception extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+
+	public DbExeception(String msg) {
+		super(msg);
+	}
+	
+	
+}
